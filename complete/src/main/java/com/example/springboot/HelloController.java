b4840000eab5b0e739891b39b515827edbc382b0
townyhawk <tom.russler42@gmail.com>
2022-10-20 18:22:38 +0200

    try something

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -3,7 +3,6 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class HelloController {
 
